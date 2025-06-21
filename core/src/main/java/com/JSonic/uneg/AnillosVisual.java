5567eb8139061385c0f85ce881f3171981d89e59
GGSO-pixel <gloriasaimans29@gmail.com>
2025-06-21 19:39:18 -0400

    items
    solo falta arreglar el iteme del anillo

--- core/src/main/java/com/JSonic/uneg/AnillosVisual.java
+++ core/src/main/java/com/JSonic/uneg/AnillosVisual.java
@@ -21,22 +21,23 @@ public class AnillosVisual extends ItemVisual {
         // Tu imagen tiene 12 anillos. Asumamos que cada uno mide 32x32 píxeles.
         // Si no, ajusta estos valores. AnchoTotal / numFrames = AnchoFrame
         // Ejemplo: si la imagen mide 384px de ancho -> 384 / 12 = 32px por frame.
-        int frameWidth = spriteSheet.getWidth() / 12;
-        int frameHeight = spriteSheet.getHeight();
+         int frameCount = 12;
+         int frameWidth = spriteSheet.getWidth() / frameCount;
+         int frameHeight = spriteSheet.getHeight();
 
         // 3. Dividir la hoja en frames individuales
         TextureRegion[][] tmp = TextureRegion.split(spriteSheet, frameWidth, frameHeight);
 
         // 4. Crear un array con los frames de la animación
 
-        int frameCount = 4;
+
         TextureRegion[] framesAnimacion = new TextureRegion[frameCount];
         for (int i = 0; i < frameCount; i++) {
             framesAnimacion[i] = tmp[0][i];
         }
 
         // Creamos la animación con una velocidad agradable (puedes ajustar 0.1f)
-        animacion = new Animation<TextureRegion>(0.1f, framesAnimacion);
+        animacion = new Animation<TextureRegion>(0.05f, framesAnimacion);
         animacion.setPlayMode(Animation.PlayMode.LOOP_PINGPONG); // <-- Ping-Pong para que vaya y vuelva suavemente
     }
 }
