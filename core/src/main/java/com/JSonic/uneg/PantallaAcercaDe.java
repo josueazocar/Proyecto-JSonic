2e46c9a0b91d1ac8bbf4ae8d98a5e09843a31157
franmbv <frannermanuel@gmail.com>
2025-06-22 19:45:47 -0400

    Creacion Acerca De y Ayuda

---
+++ core/src/main/java/com/JSonic/uneg/PantallaAcercaDe.java
@@ -0,0 +1,91 @@
+package com.JSonic.uneg;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
+
+/**
+ * Pantalla que muestra la información "Acerca De" del juego.
+ * Muestra detalles sobre el desarrollo, las herramientas y la versión.
+ */
+public class PantallaAcercaDe extends PantallaBase {
+
+    private final JSonicJuego juegoApp;
+    private Texture texturaFondo, texturaBotonVolver;
+    private BitmapFont font;
+
+    public PantallaAcercaDe(JSonicJuego juegoApp) {
+        super();
+        this.juegoApp = juegoApp;
+    }
+
+    @Override
+    public void inicializar() {
+        // --- Fondo ---
+        texturaFondo = new Texture(Gdx.files.internal("assets/Fondos/Sonic-Tails-Knuckles.png"));
+        Image imagenFondo = new Image(texturaFondo);
+        imagenFondo.setSize(mainStage.getWidth(), mainStage.getHeight());
+        mainStage.addActor(imagenFondo);
+
+        // --- Texto de "Acerca de" ---
+        font = new BitmapFont();
+        Label.LabelStyle labelStyle = new Label.LabelStyle(font, Color.WHITE);
+        String textoInfo = "JSonicJuego\n\n" +
+                           "Lenguaje de Programacion: Java\n" +
+                           "Librerias Externas: LibGDX\n\n" +
+                           "Desarrolladores:\n" +
+                           "Victor M., Jesus L., Reinaldo M.\n\n" +
+                           "Version: 1.0";
+        Label infoLabel = new Label(textoInfo, labelStyle);
+        infoLabel.setWrap(true);
+        infoLabel.setAlignment(Align.center);
+
+        // --- Botón de Volver ---
+        texturaBotonVolver = new Texture(Gdx.files.internal("assets/Botones/boton_atras.png"));
+        Button.ButtonStyle estiloBotonVolver = new Button.ButtonStyle();
+        estiloBotonVolver.up = new TextureRegionDrawable(new TextureRegion(texturaBotonVolver));
+        Button botonVolver = new Button(estiloBotonVolver);
+
+        botonVolver.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Volvemos al menú principal, mostrando los botones directamente
+                juegoApp.setPantallaActiva(new PantallaMenu(juegoApp, true));
+            }
+        });
+
+        // --- Posicionamiento ---
+        uiTable.center();
+        uiTable.add(infoLabel).width(mainStage.getWidth() * 0.8f).padBottom(40);
+
+        // --- Posicionamiento del botón Volver (en una tabla nueva) ---
+        Table tablaVolver = new Table();
+        tablaVolver.setFillParent(true);
+        uiStage.addActor(tablaVolver); // Se añade al stage de la UI
+        tablaVolver.top().left(); // Se alinea arriba a la izquierda
+        tablaVolver.add(botonVolver).size(125, 125).pad(20);
+    }
+
+    @Override
+    public void actualizar(float delta) {
+        // No se necesita lógica aquí
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        if (texturaFondo != null) texturaFondo.dispose();
+        if (texturaBotonVolver != null) texturaBotonVolver.dispose();
+        if (font != null) font.dispose();
+    }
+}
---
+++ core/src/main/java/com/JSonic/uneg/PantallaAyuda.java
@@ -0,0 +1,64 @@
+package com.JSonic.uneg;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+public class PantallaAyuda extends PantallaBase{
+    private final JSonicJuego juegoApp;
+    private Texture texturaBotonVolver;
+    public PantallaAyuda(JSonicJuego juegoApp) {
+        super();
+        this.juegoApp = juegoApp;
+    }
+
+    @Override
+    public void inicializar() {
+        // Aquí puedes inicializar los elementos de la pantalla de ayuda, como botones, fondos, etc.
+        System.out.println("Inicializando pantalla de ayuda");
+
+        Texture fondoTex = new Texture(Gdx.files.internal("Fondos/Sonic-Tails-Knuckles.png"));
+        Image fondo = new Image(fondoTex);
+        fondo.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        mainStage.addActor(fondo);
+
+        // --- Botón de Volver ---
+        texturaBotonVolver = new Texture(Gdx.files.internal("assets/Botones/boton_atras.png"));
+        Button.ButtonStyle estiloBotonVolver = new Button.ButtonStyle();
+        estiloBotonVolver.up = new TextureRegionDrawable(new TextureRegion(texturaBotonVolver));
+        Button botonVolver = new Button(estiloBotonVolver);
+
+        botonVolver.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Volvemos al menú principal, mostrando los botones directamente
+                juegoApp.setPantallaActiva(new PantallaMenu(juegoApp, true));
+            }
+        });
+
+        // --- Posicionamiento del botón Volver (en una tabla nueva) ---
+        Table tablaVolver = new Table();
+        tablaVolver.setFillParent(true);
+        uiStage.addActor(tablaVolver); // Se añade al stage de la UI
+        tablaVolver.top().left(); // Se alinea arriba a la izquierda
+        tablaVolver.add(botonVolver).size(125, 125).pad(20);
+    }
+
+    @Override
+    public void actualizar(float delta) {
+        // Aquí puedes manejar la lógica de actualización específica de la pantalla de ayuda
+        // Por ejemplo, detectar eventos de entrada o animaciones
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        if (texturaBotonVolver != null) texturaBotonVolver.dispose();
+    }
+}
