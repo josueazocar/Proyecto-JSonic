d0d905afd20d53ab5db5667c714e0f33920cd2db
franmbv <frannermanuel@gmail.com>
2025-07-02 20:08:14 -0400

    Actualizacion de informacion en AcercaDe

--- core/src/main/java/com/JSonic/uneg/PantallaAcercaDe.java
+++ core/src/main/java/com/JSonic/uneg/PantallaAcercaDe.java
@@ -1,10 +1,8 @@
 package com.JSonic.uneg;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -21,8 +19,8 @@ import com.badlogic.gdx.utils.Align;
 public class PantallaAcercaDe extends PantallaBase {
 
     private final JSonicJuego juegoApp;
-    private Texture texturaFondo, texturaBotonVolver;
-    private BitmapFont font;
+    private TextureAtlas botonesMenuAtlas;
+    private Texture texturaFondo;
 
     public PantallaAcercaDe(JSonicJuego juegoApp) {
         super();
@@ -31,61 +29,78 @@ public class PantallaAcercaDe extends PantallaBase {
 
     @Override
     public void inicializar() {
-        // --- Fondo ---
-        texturaFondo = new Texture(Gdx.files.internal("Fondos/Sonic-Tails-Knuckles.png"));
-        Image imagenFondo = new Image(texturaFondo);
-        imagenFondo.setSize(mainStage.getWidth(), mainStage.getHeight());
-        mainStage.addActor(imagenFondo);
+        // --- Cargar Atlas ---
+        botonesMenuAtlas = new TextureAtlas(Gdx.files.internal("Atlas/botonesMenu.atlas"));
 
-        // --- Texto de "Acerca de" ---
-        font = new BitmapFont();
-        Label.LabelStyle labelStyle = new Label.LabelStyle(font, Color.WHITE);
-        String textoInfo = "JSonicJuego\n\n" +
-                           "Lenguaje de Programacion: Java\n" +
-                           "Librerias Externas: LibGDX\n\n" +
-                           "Desarrolladores:\n" +
-                           "Victor M., Jesus L., Reinaldo M.\n\n" +
-                           "Version: 1.0";
-        Label infoLabel = new Label(textoInfo, labelStyle);
-        infoLabel.setWrap(true);
-        infoLabel.setAlignment(Align.center);
+        // --- Fondo ---
+        texturaFondo = new Texture(Gdx.files.internal("Fondos/Portada_desenfoque.png"));
+        Image imagenFondoPrincipal = new Image(texturaFondo);
+        imagenFondoPrincipal.setSize(mainStage.getWidth(), mainStage.getHeight());
+        mainStage.addActor(imagenFondoPrincipal);
+
+        // --- Panel de Contenido con Fondo Redondeado (¡LA NUEVA LÓGICA!) ---
+        Table panelContenido = new Table();
+        panelContenido.setBackground(getSkin().getDrawable("default-round"));
+        float anchoDelPanel = 950; // Ancho en píxeles
+        float altoDelPanel = 500;  // Alto en píxeles
+        panelContenido.setSize(anchoDelPanel, altoDelPanel);
+        panelContenido.setPosition(mainStage.getWidth() / 2, mainStage.getHeight() / 2, Align.center);
+        mainStage.addActor(panelContenido);
+
+        // --- Contenido de "Acerca de" ---
+        Label titleLabel = new Label("ACERCA DE", getSkin(), "title");
+        titleLabel.setPosition(mainStage.getWidth() / 2, mainStage.getHeight() - 200, Align.center);
+
+        Label devLabel = new Label("Desarrollado en Java con libgdx", getSkin(), "credits");
+        devLabel.setPosition(mainStage.getWidth() / 2 + 65, mainStage.getHeight() / 2 + 100, Align.center);
+        devLabel.setFontScale(0.85f);
+
+        Label creadoresLabel = new Label("Creado por: Josmer Azocar\n Josue Azocar\n Franner Bermudez\n Santiago Roman\n Gloria Saimans", getSkin(), "body");
+        creadoresLabel.setWrap(true);
+        creadoresLabel.setAlignment(Align.center);
+        creadoresLabel.setWidth(mainStage.getWidth() * 1f);
+        creadoresLabel.setPosition(mainStage.getWidth() / 2, mainStage.getHeight() / 2 - 20, Align.center);
+
+        Label versionLabel = new Label("Version: 1.0", getSkin(), "credits");
+        versionLabel.setPosition(mainStage.getWidth() / 2, mainStage.getHeight() / 2 - 140, Align.center);
+
+        mainStage.addActor(titleLabel);
+        mainStage.addActor(devLabel);
+        mainStage.addActor(creadoresLabel);
+        mainStage.addActor(versionLabel);
 
         // --- Botón de Volver ---
-        texturaBotonVolver = new Texture(Gdx.files.internal("Botones/boton_atras.png"));
         Button.ButtonStyle estiloBotonVolver = new Button.ButtonStyle();
-        estiloBotonVolver.up = new TextureRegionDrawable(new TextureRegion(texturaBotonVolver));
+        estiloBotonVolver.up = new TextureRegionDrawable(botonesMenuAtlas.findRegion("boton_atras"));
         Button botonVolver = new Button(estiloBotonVolver);
 
         botonVolver.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Volvemos al menú principal, mostrando los botones directamente
-                juegoApp.setPantallaActiva(new PantallaMenu(juegoApp, true));
+
+                PantallaMenu pantallaMenu = new PantallaMenu(juegoApp, true);
+                pantallaMenu.setEstadoMenu(PantallaMenu.EstadoMenu.OPCIONES);
+                juegoApp.setPantallaActiva(pantallaMenu);
             }
         });
-
-        // --- Posicionamiento ---
-        uiTable.center();
-        uiTable.add(infoLabel).width(mainStage.getWidth() * 0.8f).padBottom(40);
 
         // --- Posicionamiento del botón Volver (en una tabla nueva) ---
         Table tablaVolver = new Table();
         tablaVolver.setFillParent(true);
-        uiStage.addActor(tablaVolver); // Se añade al stage de la UI
-        tablaVolver.top().left(); // Se alinea arriba a la izquierda
+        uiStage.addActor(tablaVolver);
+        tablaVolver.top().left();
         tablaVolver.add(botonVolver).size(125, 125).pad(20);
     }
 
     @Override
     public void actualizar(float delta) {
-        // No se necesita lógica aquí
+
     }
 
     @Override
     public void dispose() {
         super.dispose();
+        if (botonesMenuAtlas != null) botonesMenuAtlas.dispose();
         if (texturaFondo != null) texturaFondo.dispose();
-        if (texturaBotonVolver != null) texturaBotonVolver.dispose();
-        if (font != null) font.dispose();
     }
 }
