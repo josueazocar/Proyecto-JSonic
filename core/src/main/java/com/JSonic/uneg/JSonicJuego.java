6efb777b6b80aa318ac5e82ce633824d31b62267
GGSO-pixel <gloriasaimans29@gmail.com>
2025-07-23 15:42:10 -0400

    reemplazo de efectos de sonido

--- core/src/main/java/com/JSonic/uneg/JSonicJuego.java
+++ core/src/main/java/com/JSonic/uneg/JSonicJuego.java
@@ -44,7 +44,7 @@ public class JSonicJuego extends JuegoBase {
         soundManager.registerSound("explosion_bomba", "SoundsBackground/misil.wav");
         soundManager.registerSound("golpe", "SoundsBackground/golpeGeneral.wav");
         soundManager.registerSound("habilidad_knuckles_punch", "SoundsBackground/golpeknuckle.mp3");
-        soundManager.registerSound("habilidad_Tails_punch", "SoundsBackground/robotsTails.mp3");
+        soundManager.registerSound("habilidad_Tails_punch", "SoundsBackground/robotsTails.wav");
         soundManager.registerSound("habilidad_Sonic_punch", "SoundsBackground/habilidadEspecialSonic.mp3");
 
         assetManager.finishLoading();
